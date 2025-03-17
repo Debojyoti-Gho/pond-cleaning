536ce4fb2bb69bdfce3e39224967f88c2e5da6af
Debojyoti-Gho <debojyotighoshmain@gmail.com>
2025-03-17 13:27:21 +0530

    Delete utils directory

--- utils/agent.py
+++
@@ -1,8 +0,0 @@
-def load_agent():
-    from dqn_agent import DQNAgent
-    agent = DQNAgent()
-    agent.load("agent_weights.pth")  # Load trained weights
-    return agent
-
-def decide_action(agent, obs):
-    return agent.select_action(obs)
--- utils/sam_clip.py
+++
@@ -1,6 +0,0 @@
-from .sam_clip_pipeline import segment_with_sam, classify_with_clip
-
-def run_sam_clip_pipeline(image, conf_thresh=0.65):
-    masks = segment_with_sam(image)
-    labeled_segments = classify_with_clip(image, masks, conf_thresh=conf_thresh)
-    return labeled_segments
