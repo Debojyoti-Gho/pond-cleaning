ad38dd9b7eb684b71a33375c42ffbe29bcedd2c7
Debojyoti-Gho <debojyotighoshmain@gmail.com>
2025-03-17 13:08:10 +0530

    Update sam_clip.py

--- utils/sam_clip.py
+++ utils/sam_clip.py
@@ -1,4 +1,4 @@
-from your_sam_clip_pipeline import segment_with_sam, classify_with_clip
+from .sam_clip_pipeline import segment_with_sam, classify_with_clip
 
 def run_sam_clip_pipeline(image, conf_thresh=0.65):
     masks = segment_with_sam(image)
